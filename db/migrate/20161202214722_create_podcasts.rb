39c05873ffba536ea7c48d8cfebd57551382d832
Ryan Waits <ryan.waits@gmail.com>
2016-12-02 15:51:05 -0600

    add podcast resources

---
+++ db/migrate/20161202214722_create_podcasts.rb
@@ -0,0 +1,13 @@
+class CreatePodcasts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :podcasts do |t|
+      t.string :title
+      t.text :description
+      t.string :email
+      t.string :password_digest
+      t.string :itunes
+
+      t.timestamps
+    end
+  end
+end
